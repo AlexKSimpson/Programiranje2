add1227086475653f2c33eb951ac175e7a9a38c3
AlexKSimpson <Alex.Simpson@fmf.uni-lj.si>
2020-05-12 18:01:03 +0200

    Add files via upload

---
+++ StiriKratStiri/src/vodja/Vodja.java
@@ -0,0 +1,84 @@
+package vodja;
+
+import java.util.Map;
+
+import javax.swing.SwingWorker;
+
+import splosno.Koordinati;
+import splosno.KdoIgra;
+
+import gui.GlavnoOkno;
+
+import logika.Igra;
+import logika.Igralec;
+
+import inteligenca.Inteligenca;
+
+public class Vodja {	
+	
+	public static Map<Igralec,VrstaIgralca> vrstaIgralca;
+	public static Map<Igralec,KdoIgra> kdoIgra;
+	
+	public static GlavnoOkno okno;
+	
+	public static Igra igra = null;
+	
+	public static boolean clovekNaVrsti = false;
+		
+	public static void igramoNovoIgro () {
+		igra = new Igra ();
+		igramo ();
+	}
+	
+	public static void igramo () {
+		okno.osveziGUI();
+		switch (igra.stanje()) {
+		case ZMAGA_O: 
+		case ZMAGA_X: 
+		case NEODLOCENO: 
+			return; // odhajamo iz metode igramo
+		case V_TEKU: 
+			Igralec igralec = igra.naPotezi();
+			VrstaIgralca vrstaNaPotezi = vrstaIgralca.get(igralec);
+			switch (vrstaNaPotezi) {
+			case C: 
+				clovekNaVrsti = true;
+				break;
+			case R:
+				igrajRacunalnikovoPotezo ();
+				break;
+			}
+		}
+	}
+
+	
+	public static Inteligenca racunalnikovaInteligenca = new Inteligenca();
+	
+	public static void igrajRacunalnikovoPotezo() {
+		Igra zacetkaIgra = igra;
+		SwingWorker<Koordinati, Void> worker = new SwingWorker<Koordinati, Void> () {
+			@Override
+			protected Koordinati doInBackground() {
+				Koordinati poteza = racunalnikovaInteligenca.izberiPotezo(igra);
+				return poteza;
+			}
+			@Override
+			protected void done () {
+				Koordinati poteza = null;
+				try {poteza = get();} catch (Exception e) {};
+				if (igra == zacetkaIgra) {
+					igra.odigraj(poteza);
+					igramo ();
+				}
+			}
+		};
+		worker.execute();
+	}
+		
+	public static void igrajClovekovoPotezo(Koordinati poteza) {
+		if (igra.odigraj(poteza)) clovekNaVrsti = false;
+		igramo ();
+	}
+
+
+}
